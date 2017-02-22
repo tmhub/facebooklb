75355258ef60958b8e9d11266e3c08a3c14cc9dd
Vitaliy <vitaliy@mage2cloud.com>
2017-02-22 15:24:28 +0200

    templates improvement (reuse button render code)

--- app/code/community/TM/FacebookLB/Helper/Data.php
+++ app/code/community/TM/FacebookLB/Helper/Data.php
@@ -2,29 +2,64 @@
 
 class TM_FacebookLB_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
+    protected $_config = array();
+
+    protected $_mapping = array(
+            'send'      => 'data-share',
+            'layout'    => 'data-layout',
+            'showfaces' => 'data-show-faces',
+            'width'     => 'data-width',
+            'verb'      => 'data-action',
+            'color'     => 'data-colorscheme'
+        );
+
     public function getCategoryLikeButton($product)
     {
-        $result = '';
-        if (Mage::getStoreConfig('facebooklb/category_products/enabled')) {
-            $layout = Mage::getStoreConfig('facebooklb/category_products/layout');
-            $width = Mage::getStoreConfig('facebooklb/category_products/width');
-            $action = Mage::getStoreConfig('facebooklb/category_products/verb');
-            $dataAttr = '';
-            $dataAttr .= ' data-href="' . $product->getProductUrl() . '"';
-            $dataAttr .= ' data-layout="' . $layout . '"';
-            $dataAttr .= ' data-action="' . $action . '"';
-            $dataAttr .= ' data-width="' . $width . '"';
-            if (Mage::getStoreConfig('facebooklb/category_products/send')) {
-                $dataAttr .= ' data-share="true"';
-            }
-            if (Mage::getStoreConfig('facebooklb/category_products/showfaces')) {
-                $dataAttr .= ' data-show-faces="true"';
-            }
-            if (Mage::getStoreConfig('facebooklb/category_products/color') != 'light') {
-                $dataAttr .= ' data-colorscheme="dark"';
-            }
-            $result = '<div class="fb-like"' . $dataAttr . '></div>';
+        return $this->renderLikeButton($product->getProductUrl(), 'category_products');
+    }
+
+    public function renderLikeButton($url, $configKey)
+    {
+        if (!Mage::getStoreConfig('facebooklb/general/enabled')) {
+            return '';
         }
-        return $result;
+        $config = $this->getConfig($configKey);
+        $html = '';
+        if (isset($config['enabled']) && $config['enabled'] == 'true') {
+            $dataAttr = '';
+            foreach ($this->_mapping as $key => $value) {
+                if (!isset($config[$key])) {
+                    continue;
+                }
+                $dataAttr .= sprintf(' %s="%s"', $value, $config[$key]);
+            }
+            $html = '<div class="fb-like" data-href="' . $url . '"'. $dataAttr .'></div>';
+        }
+        return $html;
+    }
+
+    public function getConfig($parentConfigKey)
+    {
+        if (!isset($this->_config[$parentConfigKey])) {
+            $config = Mage::getStoreConfig('facebooklb/' . $parentConfigKey);
+            if ($config['layout'] != 'standard') {
+                unset($config['showfaces'], $config['width']);
+            }
+            // transporm config to facebook values
+            foreach ($config as $key => $value) {
+                switch ($value) {
+                    case '1':
+                        $config[$key] = 'true';
+                        break;
+
+                    case '0':
+                        $config[$key] = 'false';
+                        break;
+                }
+            }
+            $this->_config[$parentConfigKey] = $config;
+        }
+        return $this->_config[$parentConfigKey];
     }
 }
