54286ce7d3306b8e6fea4818ae968a07bb2861a3
Vitaliy <vitaliy@mage2cloud.com>
2017-02-22 12:24:57 +0200

    system config improved (added comments)

--- app/code/community/TM/FacebookLB/Adminhtml/Model/System/Config/Source/Layout.php
+++ app/code/community/TM/FacebookLB/Adminhtml/Model/System/Config/Source/Layout.php
@@ -5,9 +5,10 @@ class TM_FacebookLB_Adminhtml_Model_System_Config_Source_Layout
     public function toOptionArray()
     {
         return array(
-            array('value'=>'button_count', 'label'=>Mage::helper('facebooklb')->__('button_count')),
-            array('value'=>'standard', 'label'=>Mage::helper('facebooklb')->__('standard')),
-            array('value'=>'box_count', 'label'=>Mage::helper('facebooklb')->__('box_count'))
+            array('value'=>'button_count', 'label'=>'button_count'),
+            array('value'=>'button', 'label'=>'button'),
+            array('value'=>'standard', 'label'=>'standard'),
+            array('value'=>'box_count', 'label'=>'box_count')
         );
     }
 }
