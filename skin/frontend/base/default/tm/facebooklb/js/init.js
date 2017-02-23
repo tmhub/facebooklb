cba9fc29d5542a25ebadde3f4e971d8a92dea07c
Vitaliy <vitaliy@mage2cloud.com>
2017-02-23 17:18:27 +0200

    fix js error like button disabled

--- skin/frontend/base/default/tm/facebooklb/js/init.js
+++ skin/frontend/base/default/tm/facebooklb/js/init.js
@@ -1,11 +1,21 @@
 document.observe("dom:loaded", function() {
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        var locale = $('fb-root').hasAttribute('data-locale')
+    // initialize facebook like button
+    var id = 'facebook-jssdk';
+    if ($(id) || !$('fb-root')) return;
+    var locale = $('fb-root').hasAttribute('data-locale')
             ? $('fb-root').getAttribute('data-locale') : 'en_US';
-        js.src = "//connect.facebook.net/"+locale+"/sdk.js#xfbml=1&version=v2.8";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    $$('script').first().insert({
+        'before': new Element('script', {
+            'src': "//connect.facebook.net/"+locale+"/sdk.js#xfbml=1&version=v2.8",
+            'id': id
+            })
+        }
+    );
+    // listen JS events
+    ["ajaxlayerednavigation:ready", "quickshopping:previewloaded",
+     "AjaxPro:onSuccess:after", "AjaxPro:onComplete:after"].map(
+        function(eventName){
+            document.observe(eventName, function(){FB.XFBML.parse()});
+        }
+    );
 });
